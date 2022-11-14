32b99fe40606fc51262f854cf1310a68cbd530ca
j_dev <j.developer.days@gmail.com>
2022-11-14 21:15:17 +0200

    create spring boot maven parent project

---
+++ spring_boot_maven_parents/bat/maven_sort_pom.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+cls
+mvn com.github.ekryd.sortpom:sortpom-maven-plugin:2.15.0:sort --file ../pom.xml -U
---
+++ spring_boot_maven_parents/shell/maven_sort_pom.sh
@@ -0,0 +1,1 @@
+clear && mvn com.github.ekryd.sortpom:sortpom-maven-plugin:2.15.0:sort --file ../pom.xml -U
