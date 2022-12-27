73611402ac74032a34f15521fd7bc7e4cccb4f91
j_dev <j.developer.days@gmail.com>
2022-12-27 21:14:26 +0200

    added webcam-capture lib

--- jdev_maven_parent/shell/maven_clean_install.sh
+++ jdev_maven_parent/shell/maven_clean_install.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 clear && mvn clean install --file ../pom.xml -U
--- jdev_maven_parent/shell/maven_sort_pom.sh
+++ jdev_maven_parent/shell/maven_sort_pom.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
 clear && mvn com.github.ekryd.sortpom:sortpom-maven-plugin:2.15.0:sort --file ../pom.xml -U
+rm -fv ../pom.xml.bak
